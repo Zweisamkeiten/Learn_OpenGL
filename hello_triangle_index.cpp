253ac586c5003e74d5d9ca1de84cf747ed9a2889
snitxmhm <snitxmhm@gmail.com>
2020-05-20 21:47:50 +0800

    两个三角形 索引绘制 线性框与填充模式

--- hello_triangle_index.cpp
+++ hello_triangle_index.cpp
@@ -1,11 +1,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <unistd.h>
 #include <iostream>
 using namespace std;
 void processInput(GLFWwindow *window);
 
 const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 1) in vec3 aPos;\n"
+    "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);"
@@ -55,25 +56,133 @@ int main()
     if(!success)
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADEr::VERTEX:COMPILAtION_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::VERTEX::COMPILAtION_FAILED\n" << infoLog << std::endl;
     }
 
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glad_glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAIED\n" << infoLog << std::endl;
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    //float vertices[] = {
+    //    // 第一个三角形
+    //    0.5f, 0.5f, 0.0f, // 右上角
+    //    0.5f, -0.5f, 0.0f, // 右下角
+    //    -0.5f, 0.5f, 0.0f, // 左上角
+    //    // 第二个三角形
+    //    0.5f, -0.5f, 0.0f,// 右下角
+    //    -0.5f, -0.5f, 0.0f, // 左下角
+    //    -0.5f, 0.5f, 0.0f // 左上角
+    //};
+    // 有几个顶点叠加了。
+    // 指定了右下角和左上角两次
+    // 一个矩形只有4个顶点而不是6个
+    // 这样产生了50%额外开销
+    // 最好的解决方案是只储存不同的顶点，并设定绘制这些顶点的顺序
+    //
+    // 因此我们需要索引缓冲对象
+    // 索引缓冲对象和顶点缓冲对象一样，EBO也是一个缓冲
+    // 它专门储存索引
+    // OpenGL调用这些顶点的索引来决定绘制哪一个顶点
+    //
+    // 索引绘制
+    // 先定义不重复的顶点，和绘制出矩形所需的索引
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f
+    };
+
+    unsigned int indices[] = { // 索引从0开始！
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+
+    unsigned int VBO, VAO;
+    unsigned int EBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // 使用索引的时候，只需定义4个顶点
+    // 创建索引缓冲对象
+    glGenBuffers(1, &EBO);
+    // 与VBO类似，我们先绑定EBO然后用glBufferData把索引复制到缓冲里
+    // 同样，也会把这些函数调用放在绑定和解绑函数调用之间
+    // 把缓冲的类型定义为GL_ELEMENT_ARRAY_BUFFER
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 传递了 GL_ELEMENT_ARRAY_BUFFER当作缓冲目标
+    // 最后使用 glDrawElements来替换glDrawArrays函数
+    // 来指明我们从索引缓冲渲染
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        // glad_glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); 如果想要恢复默认的填充模式
+        // 要想用线框模式绘制你的三角形，你可以通过glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)函数配置OpenGL如何绘制图元。第一个参数表示我们打算将其应用到所有的三角形的正面和背面，第二个参数告诉我们用线来绘制。
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+    // 最后使用 glDrawElements来替换glDrawArrays函数
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // 第一个参数指定了我们绘制的模式 与glDrawArrays一样
+        // 第二个参数为我们打算绘制顶点的个数 ，6个顶点
+        // 第三个参数为索引的类型
+        // 最后一个参数为我们指定EBO中的偏移量
+        // 或者传递一个索引数组，但是这是当我们不在使用索引缓冲对象的时候
+        // 但我们会在这里填写0
+        // glDrawElements函数从当前绑定到GL_ELEMENT_ARRAY_BUFFER目标的EBO中获取索引。这意味着我们必须在每次要用索引渲染一个物体时绑定相应的EBO，这还是有点麻烦。不过顶点数组对象同样可以保存索引缓冲对象的绑定状态。VAO绑定时正在绑定的索引缓冲对象会被保存为VAO的元素缓冲对象。绑定VAO的同时也会自动绑定EBO。
+        // 当目标是GL_ELEMENT_ARRAY_BUFFER的时候，VAO会储存glBindBuffer的函数调用。这也意味着它也会储存解绑调用，所以确保你没有在解绑VAO之前解绑索引数组缓冲，否则它就没有这个EBO配置了
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 void processInput(GLFWwindow *window)
 {
