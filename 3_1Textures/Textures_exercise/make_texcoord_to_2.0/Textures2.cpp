06e0c65e30847d219ddc817c4c2693967b8507f5
snitxmhm <snitxmhm@gmail.com>
2020-06-01 19:06:56 +0800

    texcoord_to_2.0 & change_wrap_way

---
+++ 3_1Textures/Textures_exercise/make_texcoord_to_2.0/Textures2.cpp
@@ -0,0 +1,204 @@
+#define STB_IMAGE_IMPLEMENTATION
+// 通过定义 STB_IMAGE_IMPLEMENTATION 预处理器会修改头文件
+// 让其只包含相关的函数定义源码
+// 等于是将这个头文件变为一个cpp文件
+// 现只需在程序中包含 stb_image.h 并编译就可以了
+#include "../stb_image.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+#include "../shader_s.h"
+
+using namespace std;
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Textures", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    Shader ourShader("./shader.vs", "./shader2.fs");
+
+    float vertices[] = {
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f  // top left 
+    };
+
+    // 添加了一个额外的顶点属性 所以需要告诉 OpenGL新的顶点格式
+
+    unsigned int indices[] = {  
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // 添加了一个额外的顶点属性 所以需要告诉 OpenGL新的顶点格式
+    //
+    //
+    //
+    //
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+
+    // 上一个练习创建了一个sampler2D的uniform变量
+    // 却没有使用glUniform给它赋值
+    // 使用glUniform1i可以给纹理采样器分配一个位置值
+    // 这样的话可以在一个片段着色器中设置多个纹理
+    // 一个纹理值的位置值通常称作一个纹理单元 Texture Unit
+    // 一个纹理的默认纹理单元为0
+    // 它是默认激活的纹理单元
+    // 所以上一个练习没有给其分配一个位置值
+    //
+    // 纹理单元的主要目的是为了让在着色器中可以使用多余1个的纹理
+    // 通过将纹理单元赋值给采样器 可以一次绑定多个纹理
+    // 首先需要激活相应的纹理单元
+    // 使用 glActiveTexture 激活纹理单元
+    // glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // 传入需要使用的纹理单元
+    // 激活纹理单元后，接下来的glBindTexture函数会调用和绑定这个纹理到当前激活的纹理单元
+    // 纹理单元GL_TEXTURE0 默认总是被激活
+    // 因此上一个练习中无需激活任何纹理单元
+    //
+    // OpenGL至少保证有16个纹理单元可供使用
+    // 可以激活从GL_TEXTURE0 到 GL_TEXTURE15
+    // 按顺序定义因此可以通过 GL_TEXTURE0 + 8 获得GL_TEXTURE8
+    // 当需要循环一些纹理单元时有用
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("../container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // 第一次执行发现笑脸上下颠倒了
+    // 这是因为OpenGL要求y轴0,0坐标是在图片的底部
+    // 但是图片的y轴0,0坐标通常在顶部
+    // 只要加入下面的语句就能使用stb_image.h帮助翻转y轴
+    // 在加载图像之前
+    stbi_set_flip_vertically_on_load(true);
+    data = stbi_load("../awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        // png格式使用rgba
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
+
+    // 使用glUniform1i设置每个采样器的方式告诉OpenGL每个着色器采样器属于哪个纹理单元
+    // 只需设置一次即可
+    ourShader.use();
+    // 激活着色器之前先设置uniform
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0); //手动设置
+    ourShader.setInt("texture2", 1);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        // 输入
+        processInput(window);
+
+        // 渲染
+        // 清除颜色缓冲
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        ourShader.use();
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glBindVertexArray(VAO);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // 交换缓冲并查询IO事件
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
---
+++ 3_1Textures/Textures_exercise/make_texcoord_to_2.0/shader2.fs
@@ -0,0 +1,24 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// 需要编辑片段着色器来接受另一个采样器
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+    // FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
+    FragColor = mix(texture(texture1, vec2(-TexCoord.x, TexCoord.y)), texture(texture2, vec2(-TexCoord.x, TexCoord.y)), 0.2);
+    // 最终的输出颜色现在是两个纹理的结合
+    // GLSL内建的mix函数需要接受两个值
+    // 并对它们根据第三个参数进行线性插值
+    // 如果第三个值为0.0 他会返回地一个输入
+    // 如果是1.0会返回第二个输入值
+    // 0.2会返回80% 的第一个输入颜色 和20%的第二个颜色
+    // 即返回两个纹理的混合色
+}
